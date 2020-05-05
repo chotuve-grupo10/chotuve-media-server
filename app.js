17c3e921ef57f0d8174ff94990e3d8cbb47b7448
Diego Alfonso <diego.alfonso2@gmail.com>
2020-05-05 04:18:40 -0300

    Refactor para usar el router de Express y mover los routers a una carpeta separada

--- app.js
+++ app.js
@@ -3,14 +3,13 @@
 const express = require('express');
 const app = express();
 
+const ping = require('./routes/ping');
+
 const DEFAULT_PORT = 3000;
 
-app.get('/api/ping', (req, res) => {
-  res.send(JSON.stringify({}));
-});
+app.use('/api/ping', ping);
 
 const port = process.env.PORT || DEFAULT_PORT;
-
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Para poder usar la app Express en las pruebas de integracion / aceptacion

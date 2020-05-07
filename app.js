46f53b03ba95bfd272c7c7035f156d71519fc36f
nicolaslongo <nicolaslongo@github.com>
2020-05-06 22:28:17 -0300

    modifico la respuesta al GET de ping

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
 'use strict';
 
 const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const swaggerJSDoc = require('swagger-jsdoc');
-const ping = require('./routes/ping');
-
 const app = express();
+
 const DEFAULT_PORT = 3000;
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    info: {
-      title: 'Chotuve Media Server', // Title (required)
-      version: '1.0.0', // Version (required)
-    },
-    basePath: '/api',
-  },
-  apis: ['./routes/*'], // Path to the API docs
-};
-const swaggerSpec = swaggerJSDoc(swaggerOptions);
-
-app.use('/api/ping', ping);
-// Middleware de swagger-ui-express para servir la documentacion OpenAPI
-app.use('/apidocs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get('/api/ping', (req, res) => {
+  res.send(JSON.stringify({
+  			"Health": "OK"
+  }));
+});
 
 const port = process.env.PORT || DEFAULT_PORT;
+
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Para poder usar la app Express en las pruebas de integracion / aceptacion

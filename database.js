df9ef4f318f4793d3551c900e136c47458cd0695
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-05-31 18:16:30 -0300

    Listo el swagger

--- database.js
+++ database.js
@@ -5,30 +5,34 @@ var MongoClient = mongodb.MongoClient;
 var url = process.env.MONGODB_URI;
 var db_name = process.env.DATABASE_NAME;
 
-MongoClient.connect(url, function(err, client) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', url);
+function addVideo(video_to_upload){
+  MongoClient.connect(url, function(err, client) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      console.log('Connection established to', url);
 
-    var db = client.db(db_name);
+      var db = client.db(db_name);
 
-    let data =
-        {
-          file_name: 'test',
-          file_size: '80KB',
-          created_date: '31/5/2020',
-          firebase_url: 'firebaseurl',
-          user: 'test@test.com',
-        };
+      let data =
+          {
+            file_name: 'test',
+            file_size: '80KB',
+            created_date: '31/5/2020',
+            firebase_url: 'firebaseurl',
+            user: 'test@test.com',
+          };
 
-    db.collection('videos').insertOne(data, function(err, res) {
-      if (err) throw err;
-      console.log('1 document inserted');
+      db.collection('videos').insertOne(data, function(err, res) {
+        if (err) throw err;
+        console.log('1 document inserted');
+        client.close();
+      });
+
+      // Close connection
       client.close();
-    });
+    }
+  });
+}
 
-    // Close connection
-    client.close();
-  }
-});
+module.exports = addVideo;
--- routes/upload_video.js
+++ routes/upload_video.js
@@ -2,17 +2,36 @@
 
 const express = require('express');
 const router = express.Router();
+const db = require('../database');
 
 /**
  * @swagger
  * /upload_video:
  *   post:
  *     description: Uploads a video to the server
+ *     parameters:
+ *       - in: body
+ *         name: video
+ *         description: The video to upload.
+ *         schema:
+ *           type: object
+ *           required:
+ *             - title
+ *             - url
+ *             - user
+ *           properties:
+ *             title:
+ *               type: string
+ *             url:
+ *               type: string
+ *             user:
+ *               type: string
  *     responses:
  *       200:
  *         description: video uploaded
  */
 router.post('/', (req, res) => {
+  db.addVideo({});
   res.send(JSON.stringify({Health: 'OK'}));
 });
 

ff04ba4c43436ac28676c4d2a1096175f737c355
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-05-31 16:17:47 -0300

    Anda la conexion a la base

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 COPY . .
 RUN npm install
+ENV MONGODB_URI="mongodb://heroku_tvzq5bld:mpukmuetjj1fiqfp26sm3u0u2s@ds013966.mlab.com:13966/heroku_tvzq5bld"
 EXPOSE 3000
 CMD ["npm", "run", "start"]
--- database.js
+++ database.js
@@ -1,5 +1,34 @@
-const mongoose = require('mongoose');
+const mongodb = require('mongodb');
 
-mongoose.connect('mongodb://localhost/mediaserverdb', {useUnifiedTopology: true, useNewUrlParser : true})
-.then(db => console.log("DB connected"))
-.catch(err => console.log(err));
+var MongoClient = mongodb.MongoClient;
+var url = process.env.MONGODB_URI;
+
+MongoClient.connect(url, function (err, client) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      console.log('Connection established to', url);
+
+      var db = client.db("heroku_tvzq5bld");
+
+      let data =
+        {
+          file_name: 'test',
+          file_size: '80KB',
+          created_date: '31/5/2020',
+          firebase_url: 'firebaseurl',
+          user: 'test@test.com'
+        };
+
+      db.collection("videos").insertOne(data, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        client.close();
+      });
+
+
+
+      //Close connection
+      client.close();
+    }
+});

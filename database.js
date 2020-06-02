1ad70351829b95cef9b7672fd44e4698d6e76319
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-06-02 15:48:29 -0300

    Mas cosas del linter

--- database.js
+++ database.js
@@ -41,71 +41,71 @@ var getAllVideos = async function(callback){
       console.log('Connection established to', url);
 
       var db = client.db(db_name);
-      
-      //Busca todos los videos
+
+      // Busca todos los videos
       db.collection('videos').find({}).toArray(function(err, result) {
-          if (err) throw err;
-          callback(null,result);
-          client.close();
-        });
-
-      // Close connection
-      client.close();
-  }
-});
-}
-
-var getAllVideosForUser = async function(user_p,callback){
-  return MongoClient.connect(url, function(err, client) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', url);
-
-    var db = client.db(db_name); 
-    //Crea la query con el user   
-    var query = { user: user_p };
-    //Busca los videos asociados al user
-    db.collection('videos').find(query).toArray(function(err, result) {
         if (err) throw err;
-        callback(null,result);
+        callback(null, result);
         client.close();
       });
 
-    // Close connection
-    client.close();
-  }
-});
-}
-
-var deleteVideoById = async function(id,callback){
-  return MongoClient.connect(url, function(err, client) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', url);
-
-    var db = client.db(db_name); 
-    //Crea la query con el user   
-    var query = { _id: id };
-    //Busca los videos asociados al user
-    db.collection('videos').findOneAndDelete(query, function(err, res){
-      if (err){
-        console.log('Unable to delete document to the mongoDB server. Error:', err);
-        throw err;
-      }
-      console.log(res);
-      console.log("Number of records deleted: " + res.affectedRows);
+      // Close connection
       client.close();
-    
-    });
+    }
+  });
+};
 
-    // Close connection
-    client.close();
-  }
-});
-}
+var getAllVideosForUser = async function(user_p, callback){
+  return MongoClient.connect(url, function(err, client) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      console.log('Connection established to', url);
 
+      var db = client.db(db_name);
+      // Crea la query con el user
+      var query = { user: user_p };
+      // Busca los videos asociados al user
+      db.collection('videos').find(query).toArray(function(err, result) {
+        if (err) throw err;
+        callback(null, result);
+        client.close();
+      });
+
+      // Close connection
+      client.close();
+    }
+  });
+};
+
+var deleteVideoById = async function(id, callback){
+  return MongoClient.connect(url, function(err, client) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      console.log('Connection established to', url);
+
+      var db = client.db(db_name);
+      // Crea la query con el user
+      var query = { _id: id };
+      // Busca los videos asociados al user
+      db.collection('videos').findOneAndDelete(query, function(err, res){
+        if (err){
+          // eslint-disable-next-line max-len
+          console.log('Unable to delete document to the mongoDB server. Error:', err);
+          throw err;
+        }
+        console.log(res);
+        console.log('Number of records deleted: ' + res.affectedRows);
+        client.close();
+
+      });
+
+      // Close connection
+      client.close();
+    }
+  });
+};
 
 
 module.exports.getAllVideos = getAllVideos;

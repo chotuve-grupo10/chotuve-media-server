e1c3dbe84b16db6b9b3a25f1677eb6274b85c6c4
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-06-10 13:35:49 -0300

    Todos los metodos de DB refactorizados

--- Database.js
+++ Database.js
@@ -91,36 +91,4 @@ class Database{
   };
 }
 
-// async function addVideo(video_to_upload){
-//   MongoClient.connect(url, function(err, client) {
-//     if (err) {
-//       console.log('Unable to connect to the mongoDB server. Error:', err);
-//       throw err;
-//     } else {
-//       console.log('Connection established to', url);
-
-//       var db = client.db(db_name);
-
-//       var datetime = new Date();
-//       video_to_upload['upload_date'] = datetime.toLocaleDateString();
-
-//       console.log('Document to insert: ' + JSON.stringify(video_to_upload));
-
-//       db.collection('videos').insertOne(video_to_upload, function(err, res) {
-//         if (err) {
-//           // eslint-disable-next-line max-len
-// eslint-disable-next-line max-len
-//           console.log('Unable to insert document to the mongoDB server. Error:', err);
-//           throw err;
-//         }
-//         console.log('1 document inserted');
-//         client.close();
-//       });
-
-//       // Close connection
-//       client.close();
-//     }
-//   });
-// }
-
 module.exports.Database = Database;

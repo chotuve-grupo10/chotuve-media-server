e9a8cd8ce0fd184e319a4bee9fc57b9641e63565
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-06-30 21:17:10 -0300

    Listo el get para un token en particular

--- routes/app_servers.js
+++ routes/app_servers.js
@@ -139,4 +139,82 @@ router.get('/', async(req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /app_servers/{media_server_token}:
+ *   get:
+ *     tags:
+ *       - name: Admin users only
+ *     description: Returns registered app servers
+ *     parameters:
+ *       - in: path
+ *         name: media_server_token
+ *         description: look for specific server
+ *         type: string
+ *         required: true
+ *       - in: header
+ *         name: authorization
+ *         description: Admin user token
+ *         type: string
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: returns app servers array.
+ *         schema:
+ *           $ref: '#/definitions/AppServer'
+ *       403:
+ *         description: you are not an admin user.
+ *         schema:
+ *           $ref: '#/definitions/ErrorResponse'
+ *       500:
+ *         description: there is an internal problem with the media server.
+ *         schema:
+ *           $ref: '#/definitions/ErrorResponse'
+ * definitions:
+ *   AppServer:
+ *     type: object
+ *     properties:
+ *       _id:
+ *         type: string
+ *         example: 5da454f4307b0a8b3083883b
+ *       token:
+ *         type: string
+ *         format: uuid
+ *       registered_at:
+ *         type: string
+ *         format: date-time
+ *   ErrorResponse:
+ *     type: object
+ *     properties:
+ *       Error:
+ *         type: string
+ *         example: Error message
+ */
+
+router.get('/:media_server_token', async(req, res) => {
+  // eslint-disable-next-line max-len
+  if (!token_functions.is_valid_token_from_admin_user(req.get(AUTHORIZATION_HEADER))){
+    console.log('Token is NOT from admin user');
+    res.status(403).send({Error: 'Request doesnt come from an admin user'});
+  } else {
+    const db_service = new MongoDB();
+    var db;
+    await db_service.start();
+
+    db = new AppServersCollection(db_service.db);
+
+    // eslint-disable-next-line max-len
+    await db.getAppServerWithToken(req.params.media_server_token, function(err, appServersList){
+      if (err) {
+        console.log(err);
+        res.status(500).send({Error: err.message});
+        db_service.stop();
+      }
+      res.send(appServersList);
+      db_service.stop();
+    });
+  }
+});
+
+
 module.exports = router;

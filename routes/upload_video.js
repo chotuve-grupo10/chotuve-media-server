48e0a78ecd782dfb0b3e55269fc37a2e861a62be
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-07-01 12:44:30 -0300

    Se chequea el media server token en upload video

--- routes/upload_video.js
+++ routes/upload_video.js
@@ -4,13 +4,24 @@ const express = require('express');
 const router = express.Router();
 const Database = require('../Database').Database;
 const MongoDB = require('../MongoDB').MongoDB;
+// eslint-disable-next-line max-len
+const AppServersCollection = require('../db/AppServersCollection').AppServersCollection;
+const validation_functions = require('../utilities/validation_functions');
+
+const MEDIA_SERVER_TOKEN_HEADER = 'MediaServerToken';
 
 /**
  * @swagger
  * /upload_video:
  *   post:
+ *     tags:
+ *       - name: All users
  *     description: Uploads a video to the server
  *     parameters:
+ *       - in: header
+ *         name: MediaServerToken
+ *         type: string
+ *         required: true
  *       - in: body
  *         name: video
  *         description: The video to upload.
@@ -40,28 +51,52 @@ const MongoDB = require('../MongoDB').MongoDB;
  *     responses:
  *       201:
  *         description: return _id of the inserted document.
+ *       403:
+ *         description: your media server token is invalid.
+ *         schema:
+ *           $ref: '#/definitions/ErrorResponse'
  *       500:
  *         description: there is an internal problem with the media server.
+ *         schema:
+ *           $ref: '#/definitions/ErrorResponse'
  */
 router.post('/', async(req, res) => {
 
   const db_service = new MongoDB();
-  var db;
+  var db, app_servers_collection;
   await db_service.start();
 
-  db = new Database(db_service.db);
+  app_servers_collection = new AppServersCollection(db_service.db);
 
-  await db.addVideo(req.body, function(err, insertedDocument){
-    if (err){
+  // eslint-disable-next-line max-len
+  await validation_functions.is_valid_media_server_token(req.get(MEDIA_SERVER_TOKEN_HEADER), app_servers_collection, async function(err, is_valid_token){
+    if (err) {
       console.log(err);
       res.status(500).send({Error: err.message});
-    } else {
-      // eslint-disable-next-line max-len
-      res.status(201).send(JSON.stringify({_id: insertedDocument.insertedId}));
+      db_service.stop();
     }
-  }).catch(e => {
-    res.status(500).send({Error: e.message});
-    console.log('Error: ', e.message);
+
+    if (!is_valid_token){
+      console.log('Invalid media server token');
+      res.status(403).send({Error: 'Invalid media server token'});
+      db_service.stop();
+    } else {
+      console.log('Valid media server token');
+      db = new Database(db_service.db);
+
+      await db.addVideo(req.body, function(err, insertedDocument){
+        if (err){
+          console.log(err);
+          res.status(500).send({Error: err.message});
+        } else {
+          // eslint-disable-next-line max-len
+          res.status(201).send(JSON.stringify({_id: insertedDocument.insertedId}));
+        }
+      }).catch(e => {
+        res.status(500).send({Error: e.message});
+        console.log('Error: ', e.message);
+      });
+    }
   });
 });
 

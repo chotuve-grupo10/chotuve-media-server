a9559ccd3b30dea335277d98415291b9dbb1785f
nicolaslongo <nicolaslongo@github.com>
2020-07-29 21:04:26 -0300

    Borrado lo viejo

--- Database.js
+++ Database.js
@@ -57,18 +57,6 @@ class Database{
     return this.collection.find(query).limit(max_results).toArray();
   }
 
-  async getAllVideosForUser(user_p, callback){
-
-    console.log('Looking videos from user ' + user_p);
-    // Crea la query con el user
-    var query = { user: user_p };
-    // Busca los videos asociados al user
-    this.collection.find(query).toArray(function(err, result) {
-      if (err) throw err;
-      callback(null, result);
-    });
-  };
-
   async getVideoById(id, callback){
 
     if (mongodb.ObjectID.isValid(id)){
@@ -89,31 +77,6 @@ class Database{
       callback(null, null);
     }
   };
-
-  /**
-   * @deprecated
-   * @param {*} id
-   * @param {*} callback
-   */
-  async deleteVideoById(id, callback){
-    if (mongodb.ObjectID.isValid(id)){
-      console.log('The id received is valid');
-      var query = { _id: mongodb.ObjectID(id) };
-      // Busca los videos asociados al user
-      this.collection.findOneAndDelete(query, function(err, res){
-        if (err){
-          // eslint-disable-next-line max-len
-          console.log('Unable to delete document to the mongoDB server. Error:', err);
-          throw err;
-        }
-        console.log('Number of records deleted: ' + res.affectedRows);
-        callback(null, res);
-      });
-    } else {
-      console.log('Invalid ID received to delete');
-      callback('Invalid ID received to delete', null);
-    }
-  }
 
   async deleteVideoByObjectId(id){
     return new Promise((resolve, reject) => {
--- test/db/tests_database.js
+++ test/db/tests_database.js
@@ -98,72 +98,6 @@ describe('Database', function() {
   });
 
   describe('List videos', function() {
-    it('should return no videos for non existing user', function(done) {
-
-      let video_to_add = {
-        description: 'fsaf',
-        fileName: '5b2bb692-46ab-4bc4-aefc-ac9cd9b97b0c',
-        isPrivate: false,
-        latitude: '-72.544969',
-        longitude: '-13.163175',
-        title: 'fgsdf',
-        url: 'test',
-        user: 'diegote@gmail.com',
-        upload_date: '6/7/2020',
-      };
-
-      db_test.addVideo(video_to_add, function(err, videoInserted){
-        if (err) {
-          console.log(err);
-          done(err);
-        }
-
-        db_test.getAllVideosForUser('test', function(err, videosList){
-          if (err) {
-            console.log(err);
-            done(err);
-          }
-
-          expect(videosList.length).to.be.eq(0);
-          done();
-        });
-      });
-    });
-
-    it('should return one video for existing user', function(done) {
-
-      let video_to_add = {
-        description: 'fsaf',
-        fileName: '5b2bb692-46ab-4bc4-aefc-ac9cd9b97b0c',
-        isPrivate: false,
-        latitude: '-72.544969',
-        longitude: '-13.163175',
-        title: 'fgsdf',
-        url: 'test',
-        user: 'diegote@gmail.com',
-        upload_date: '6/7/2020',
-      };
-
-      db_test.addVideo(video_to_add, function(err, videoInserted){
-        if (err) {
-          console.log(err);
-          done(err);
-        }
-
-        // eslint-disable-next-line max-len
-        db_test.getAllVideosForUser('diegote@gmail.com', function(err, videosList){
-          if (err) {
-            console.log(err);
-            done(err);
-          }
-
-          expect(videosList.length).to.be.eq(1);
-          expect(videosList[0].user).to.be.eq('diegote@gmail.com');
-          done();
-        });
-      });
-    });
-
     it('should return one video for id', function(done) {
 
       let video_to_add = {
@@ -289,57 +223,6 @@ describe('Database', function() {
       var deleted =
         await db_test.deleteVideoByObjectId('5f1fa6cc27db6d00100e9e31');
       expect(deleted).to.be.null;
-    });
-  });
-
-  describe('Delete video', function() {
-    it('should delete only video so list is empty', function(done) {
-
-      let video_to_add = {
-        description: 'fsaf',
-        fileName: '5b2bb692-46ab-4bc4-aefc-ac9cd9b97b0c',
-        isPrivate: false,
-        latitude: '-72.544969',
-        longitude: '-13.163175',
-        title: 'fgsdf',
-        url: 'test',
-        user: 'diegote@gmail.com',
-        upload_date: '6/7/2020',
-      };
-
-      db_test.addVideo(video_to_add, function(err, videoInserted){
-        if (err) {
-          console.log(err);
-          done(err);
-        }
-
-        db_test.getAllVideos(function(err, videosList){
-          if (err) {
-            console.log(err);
-            done(err);
-          }
-
-          expect(videosList.length).to.be.eq(1);
-          let id = videosList[0]._id;
-
-          db_test.deleteVideoById(id, async function(err, video){
-            if (err){
-              console.log(err);
-              done(err);
-            }
-
-            db_test.getAllVideos(function(err, videos){
-              if (err){
-                console.log(err);
-                done(err);
-              }
-
-              expect(videos.length).to.be.eq(0);
-              done();
-            });
-          });
-        });
-      });
     });
   });
 });
